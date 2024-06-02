9bee4cc1677b54395a1c74ea810b5930416718d1
MRamirez202210582 <U202210582@upc.edu.pe>
2024-06-02 00:24:51 -0500

    feature model commands

--- NexaLibery-Backend.API/MultimediaContent/Domain/Model/Commands/CreateMultimediaCommand.cs
+++ NexaLibery-Backend.API/MultimediaContent/Domain/Model/Commands/CreateMultimediaCommand.cs
@@ -1,3 +1,3 @@
 ﻿namespace NexaLibery_Backend.API.MultimediaContent.Domain.Model.Commands;
 
-public record  CreateMultimediaCommand();
+public record CreateMultimediaCommand(string title, string description ,string pic, string url,string premium);
