1b6a05647e78feded17c9bd18f19b4b32ceff50f
MRamirez202210582 <U202210582@upc.edu.pe>
2024-06-02 00:22:21 -0500

    feature model aggregates

--- NexaLibery-Backend.API/MultimediaContent/Domain/Model/Aggregates/Multimedia.cs
+++ NexaLibery-Backend.API/MultimediaContent/Domain/Model/Aggregates/Multimedia.cs
@@ -1,6 +1,26 @@
-﻿namespace NexaLibery_Backend.API.MultimediaContent.Domain.Model.Entities;
+﻿using NexaLibery_Backend.API.MultimediaContent.Domain.Model.Commands;
+namespace NexaLibery_Backend.API.MultimediaContent.Domain.Model.Aggregates;
 
 public class Multimedia
 {
+    public int id { get; private set; }
+    public string title { get; private set; }
+    public string description { get; private set; }
+    public DateTime date { get; private set; }
+    public string pic { get; private set; }
+    public string url { get; private set; }
+    public string premium { get; private set; }
     
+    public Multimedia()
+    {
+    }
+    
+    public Multimedia(CreateMultimediaCommand command)
+    {
+        this.title = command.title;
+        this.description = command.description;
+        this.pic = command.pic;
+        this.url = command.url;
+        this.premium = command.premium;
+    }
 }
--- NexaLibery-Backend.API/MultimediaContent/Domain/Model/Aggregates/Podcast.cs
+++ NexaLibery-Backend.API/MultimediaContent/Domain/Model/Aggregates/Podcast.cs
@@ -1,6 +1,28 @@
-﻿namespace NexaLibery_Backend.API.MultimediaContent.Domain.Model.Entities;
+﻿using NexaLibery_Backend.API.MultimediaContent.Domain.Model.Commands;
+
+namespace NexaLibery_Backend.API.MultimediaContent.Domain.Model.Aggregates;
 
 public class Podcast
 {
+    public int id { get; private set; }
+    public string title { get; private set; }
+    public string description { get; private set; }
+    public DateTime date { get; private set; }
+    public string pic { get; private set; }
+    public string url { get; private set; }
+    public string premium { get; private set; }
+
+    public Podcast()
+    {
+        
+    }
+    public Podcast(CreatePodcastCommand command)
+    {
+        this.title = command.title;
+        this.description = command.description;
+        this.pic = command.pic;
+        this.url = command.url;
+        this.premium = command.premium;
+    }
     
 }
